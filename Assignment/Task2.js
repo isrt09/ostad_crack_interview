ad4c7268ac44db460ee2330d2a9136aa4c89edcc
isrt09 <mazedur@gmail.com>
2024-07-09 21:16:16 +0600

    Task 02 is completed done

---
+++ Assignment/Task2.js
@@ -0,0 +1,7 @@
+function strStr(haystack, needle) {
+    return haystack.indexOf(needle);
+}
+
+// Test Output
+console.log(strStr("sadbutsad", "sad")); // Output: 0
+console.log(strStr("codemama", "ostad")); // Output: -1
