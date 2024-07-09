a137ba94cf54e21fa3d845e692638ff453f934d9
isrt09 <mazedur@gmail.com>
2024-07-09 21:19:31 +0600

    Task 02 Time & Space Complexity solution added

--- Assignment/Task2.js
+++ Assignment/Task2.js
@@ -5,3 +5,9 @@ function strStr(haystack, needle) {
 // Test Output
 console.log(strStr("sadbutsad", "sad")); // Output: 0
 console.log(strStr("codemama", "ostad")); // Output: -1
+
+// Time Complexity:
+The indexOf method typically has a time complexity of O(n + m), where n is the length of haystack and m is the length of needle.
+
+// Space Complexity:
+The space complexity of this approach is O(1)
