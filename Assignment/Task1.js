76687ccab83d083aefa0a7950f66f8815b3b9b0d
isrt09 <mazedur@gmail.com>
2024-07-09 20:55:27 +0600

    Time & Space Complexity solution added

--- Assignment/Task1.js
+++ Assignment/Task1.js
@@ -21,3 +21,10 @@ const indices3 = [1,0,2];
 console.log(shuffleString(s1, indices1)); 
 console.log(shuffleString(s2, indices2)); 
 console.log(shuffleString(s3, indices3)); 
+
+// Time Complexity:
+The time complexity of this solution is O(n), where n is the length of the string s or the number of elements in indices. 
+This is because we iterate through each character of s once to place it correctly in the shuffled array.
+
+// Space Complexity:
+The space complexity is O(n) due to the extra space used to store the shuffled array.
