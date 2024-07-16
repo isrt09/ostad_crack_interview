c0c2eff0402dbaa67b05d72f4f974353ebc4d0fa
isrt09 <mazedur@gmail.com>
2024-07-16 21:42:50 +0600

    Task 02 Time & Space Complexity added

--- Assignment/Task02.js
+++ Assignment/Task02.js
@@ -10,3 +10,9 @@ function multiply(a, b) {
 console.log(multiply(4, 5));  // Output: 20
 console.log(multiply(3, 3));  // Output: 30
 console.log(multiply(0, 2));  // Output: 56
+
+Time Complexity:
+The time complexity of this recursive solution is O(b), where b is the value of the second integer. This is because the function recursively adds a b times.
+
+Space Complexity:
+The space complexity is O(b) due to the recursion stack used by the function calls. In the worst case, the recursion depth could go up to b.
