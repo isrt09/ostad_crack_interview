bdec587eae00c887a1b143e006e7f63fd4d1fba4
isrt09 <mazedur@gmail.com>
2024-08-28 20:50:40 +0600

    added Time & Space Complexity

--- Task02.js
+++ Task02.js
@@ -19,3 +19,6 @@ const y = "azxxzy";
 console.log(removeDuplicates(x));
 console.log(removeDuplicates(y));
 
+// Time and Space Complexity:
+Time Complexity: O(n), where n is the length of the string. Each character is processed once and each stack operation (push or pop) is O(1). Thus, the overall time complexity is linear.
+Space Complexity: O(n), where n is the length of the string. In the worst case, all characters might be pushed onto the stack if no adjacent duplicates are present. Therefore, the space complexity is linear with respect to the size of the input string.
