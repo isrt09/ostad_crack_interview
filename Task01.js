a9d16a8d32c49c8332d5945dd253c8fead7303e7
isrt09 <mazedur@gmail.com>
2024-08-28 20:44:30 +0600

    add Time & Space Complexity

--- Task01.js
+++ Task01.js
@@ -27,7 +27,9 @@ console.log(isValid(s));
 console.log(isValid(a));
 console.log(isValid(b));
 
-
+// Time and Space Complexity:
+Time Complexity: O(n), where n is the length of the string. Each character is processed once, and each stack operation (push and pop) is O(1).
+Space Complexity: O(n) in the worst case, where n is the length of the string. This happens when all characters are opening brackets, and they are stored in the stack.
 
 
 
