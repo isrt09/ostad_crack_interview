014d5d086550046f64fe6734e1b7065a8f68cc5b
Mazedur <mazedur@gmail.com>
2024-09-11 21:29:58 +0600

    Task01 is done for Searching

---
+++ Task01.js
@@ -0,0 +1,22 @@
+function binarySearch(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (nums[mid] === target) {
+            return mid; // Target found
+        } else if (nums[mid] < target) {
+            left = mid + 1; // Target is in the right half
+        } else {
+            right = mid - 1; // Target is in the left half
+        }
+    }
+
+    return -1; // Target not found
+}
+
+// Test Output Result
+const nums = [-1,0,3,5,9,12], target = 9
+console.log(binarySearch(nums,target))
